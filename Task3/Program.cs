7f0523e8c04bfc03f0f8ef2e6eeddc87bf122ad2
DAYNYUS <excaliburbmw@mail.ru>
2022-06-10 07:35:12 +0200

    add task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,9 +1,19 @@
-﻿//ДЗ3 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет(645->5, 78->третьей цифры нет, 32679->6)
+﻿//ДЗ2 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет(645->5, 78->третьей цифры нет, 32679->6)
+//1
+//int Number = new Random().Next(1, 1500);
+//System.Console.WriteLine();
+//System.Console.WriteLine($"Дано число: {Number}");
 
-Console.Write("Введите число: ");
-string input = Console.ReadLine();
-int number = Convert.ToInt32(input);
+//if (Number / 100 > 0) System.Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 100 % 10}");
+//else System.Console.WriteLine($"У числа {Number} нет третьей цифры");
 
-int a = number % 10;
+//System.Console.WriteLine();
 
-Console.WriteLine("Последняя цифра этого числа: " + a);
+//2
+//Console.Write("Введите число: ");
+//string input = Console.ReadLine();
+//int number = Convert.ToInt32(input);
+
+//int a = number % 10;
+
+//Console.WriteLine("Последняя цифра этого числа: " + a);
