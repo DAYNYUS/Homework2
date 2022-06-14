db01c98b3d2dc0c4c51d4e73cf28e4b796407067
DAYNYUS <106028605+DAYNYUS@users.noreply.github.com>
2022-06-14 13:45:36 +0200

    Update Program.cs

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,14 +1,5 @@
 ﻿//Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа (456->5, 782->8, 918->1)
 
-//System.Console.WriteLine();
-//int number = new Random().Next(100, 1000);
-
-//System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
-
-//System.Console.WriteLine(); 
-
-
-
 Console.Write("Введите трехзначное число: ");
 string input = Console.ReadLine();
 int number = Convert.ToInt32(input);
